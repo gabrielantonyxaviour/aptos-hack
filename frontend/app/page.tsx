5cf9c06548772798dd23c58165294751308a2d6d
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2024-10-01 19:58:21 +0530

    switch theme works

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import ToggleTheme from "@/components/custom/toggle-theme";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,6 +14,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <ToggleTheme />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
